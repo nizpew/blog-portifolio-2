8780decb4fd36a16253a24ab0e6ee136c9bde814
nizpew <profissionalArb@proton.me>
2024-11-27 02:44:33 -0400

    initial commits

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-
-export default defineConfig({
-	integrations: [tailwind(), mdx()],
-});
